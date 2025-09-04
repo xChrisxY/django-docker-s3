13a312f93ad66a100544ba1cb6e47065b0d69fa4
xChrisxY <christopheryahirm@gmail.com>
2025-09-03 18:35:03 -0600

    views/event

--- app/events/views.py
+++ app/events/views.py
@@ -1,3 +1,61 @@
-from django.shortcuts import render
+from rest_framework import viewsets, status
+from rest_framework.parsers import MultiPartParser, FormParser, JSONParser
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.exceptions import PermissionDenied
+from rest_framework.response import Response
+from django.db.models import Q
+from app.item.models import ItemList
+from app.item.serializers import ItemListSerializer
 
-# Create your views here.
+from .models import Event, AudioNote, EventImage
+from .serializers import EventSerializer, EventImageSerializer, AudioNoteSerializer
+
+class EventViewSet(viewsets.ModelViewSet): 
+
+    queryset = Event.objects.all()
+    serializer_class = EventImageSerializer 
+    parser_classes = [MultiPartParser, FormParser, JSONParser]
+
+    def get_permissions(self): 
+        if self.action in ['create', 'update', 'partial_update', 'destroy']:
+            return [IsAuthenticated()]
+        return [AllowAny()]
+
+    def get_queryset(self): 
+        # sobreescribimos el método para mostrar los eventos organizados o en los que participa el cliente.
+        user = self.request.user 
+        if user.is_authenticated: 
+            events = Event.objects.filter(Q(organizer=user) | Q(participants=user)).distinct()
+            return events 
+
+        raise PermissionDenied("Usuario no autenticado. Por favor, incluye un token de acceso")
+
+    @action(detail=True, methods=['POST'], permission_classes=[IsAuthenticated])
+    def add_image(self, request, pk=None):
+        # Personalizamos la acción para añadir imágenes a un evento 
+        event = self.get_object()
+        serializer = EventImageSerializer(data=request.data) 
+
+        if serializer.is_valid(): 
+            serializer.save(event=event) # Guardamos la imágen asociada al evento 
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    @action(detail=True, methods=['POST'], permission_classes=[IsAuthenticated])
+    def add_note_audio(self, request, pk=None): 
+        event = self.get_object()
+        serializer = AudioNoteSerializer(data=request.data)
+
+        if serializer.is_valid(): 
+            serializer.save(event=event)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    @action(detail=True, methods=['GET'], permission_classes=[IsAuthenticated])
+    def item_list(self, request, pk=None): 
+        event = self.get_object()
+        item_list, created = ItemList.objects.get_or_create(event=event)
+        serializer = ItemListSerializer(item_list)
+        return Response(serializer.data)
+
--- app/items/views.py
+++ app/items/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from rest_framework import viewsets 
+from rest_framework.permissions import AllowAny, IsAuthenticated
 
-# Create your views here.
+from .models import Item, ItemList 
+from .serializers import ItemSerializer
+
+class ItemViewSet(viewsets.ModelViewSet):
+
+    queryset = Item.objects.all()
+    serializer_class = ItemSerializer 
+
+    def get_permissions(self): 
+        # Personalizamos los permisos 
+        if self.action in ['create', 'update', 'partial_update', 'destroy']:
+            return [IsAuthenticated()]
+        # Para list y retrieve (acciones de lectura)
+        return [AllowAny()]
+
+    def get_queryset(self):
+        # Filtrar por elementos por eventos si se proporciona
+        event_id = self.request.query_params.get('event', None)
+        if event_id:
+            items = Item.objects.filter(item_list__event_id=event_id)
+            return items 
+        return Item.objects.none()
+
+
+
+
